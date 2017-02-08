7bb2777822c8e21b488ac724b686396d50676f64
Jakub Mareda <mareda@techsys.cz>
2017-02-08 16:11:19 +0100

    First commit

---
+++ course/test1/main.c
@@ -0,0 +1,11 @@
+/**
+ * Nucleo program that does not do anything. Test of basic compilation. 
+ * 
+ * arm-none-eabi-gcc main.c --specs=nosys.specs -L../../software/mbed-os/
+**/
+
+#include <mbed.h>
+void main()
+{
+
+}
