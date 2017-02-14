bc9898603933b5059f0380296ccf16ef67929ddd
Jakub Mareda <mareda@techsys.cz>
2017-02-14 15:59:47 +0100

    It doesn't work. It never will, abadonning this.

--- course/test1/main.c
+++ course/test1/main.c
@@ -1,10 +1,11 @@
 /**
  * Nucleo program that does not do anything. Test of basic compilation. 
  * 
- * arm-none-eabi-gcc main.c --specs=nosys.specs -L../../software/mbed-os/
+ * arm-none-eabi-gcc main.c --specs=nosys.specs -I../../software/mbed-os/ -I"C:/Program Files (x86)/GNU Tools ARM Embedded/5.4 2016q3/arm-none-eabi/include/c++/5.4.1/"
 **/
 
-#include <mbed.h>
+//#include <mbed.h>
+#include <cstddef>
 void main()
 {
 
